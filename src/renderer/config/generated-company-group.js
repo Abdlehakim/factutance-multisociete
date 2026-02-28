587ad626b9fccf987a70e58d47cc06dd52892b3a
hakim <contact@smartwebify.com>
2026-02-28 01:35:47 +0100

    update

--- src/renderer/config/generated-company-group.js
+++ src/renderer/config/generated-company-group.js
@@ -1,5 +1,5 @@
 // AUTO-GENERATED. Do not commit.
 (function(g){
-  g.__FACTURANCE_BUILD_COMPANY_GROUP__ = {"mode":"group","groupId":"wellcomgroup","groupName":"WELLCOM GROUP","companies":["WELLCOM","ProDisplay"],"createdAt":"2026-02-27T03:29:51.342Z"};
+  g.__FACTURANCE_BUILD_COMPANY_GROUP__ = {"mode":"group","groupId":"wellcomgroup","groupName":"WELLCOM GROUP","companies":["WELLCOM","ProDisplay"],"createdAt":"2026-02-28T00:25:14.188Z"};
   if (typeof module !== "undefined" && module.exports) module.exports = g.__FACTURANCE_BUILD_COMPANY_GROUP__;
 })(typeof window !== "undefined" ? window : (typeof global !== "undefined" ? global : this));
