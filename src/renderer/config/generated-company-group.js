2283fda43dacc67e02068c7af4e2bde0cddfc202
unknown <hakimsabkhi001@gmail.com>
2026-03-12 07:21:33 +0100

    update

--- src/renderer/config/generated-company-group.js
+++ src/renderer/config/generated-company-group.js
@@ -1,5 +1,5 @@
 // AUTO-GENERATED. Do not commit.
 (function(g){
-  g.__FACTURANCE_BUILD_COMPANY_GROUP__ = {"mode":"single","groupId":"","groupName":"","companies":[],"createdAt":"2026-03-11T06:54:30.668Z"};
+  g.__FACTURANCE_BUILD_COMPANY_GROUP__ = {"mode":"single","groupId":"","groupName":"","companies":[],"createdAt":"2026-03-11T20:20:00.121Z"};
   if (typeof module !== "undefined" && module.exports) module.exports = g.__FACTURANCE_BUILD_COMPANY_GROUP__;
 })(typeof window !== "undefined" ? window : (typeof global !== "undefined" ? global : this));
