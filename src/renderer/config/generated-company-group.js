e1ef6c63622e15b637bc358570ff55431b4bd006
unknown <hakimsabkhi001@gmail.com>
2026-04-05 21:15:49 +0100

    update

--- src/renderer/config/generated-company-group.js
+++ src/renderer/config/generated-company-group.js
@@ -1,5 +1,5 @@
 // AUTO-GENERATED. Do not commit.
 (function(g){
-  g.__FACTURANCE_BUILD_COMPANY_GROUP__ = {"mode":"group","groupId":"smartwebify-groupe","groupName":"SmartWebify Groupe","companies":["SmartWebify","Facturance"],"createdAt":"2026-03-29T15:45:28.309Z"};
+  g.__FACTURANCE_BUILD_COMPANY_GROUP__ = {"mode":"group","groupId":"smartwebify-groupe","groupName":"SmartWebify Groupe","companies":["SmartWebify","Facturance"],"createdAt":"2026-04-05T08:04:31.380Z"};
   if (typeof module !== "undefined" && module.exports) module.exports = g.__FACTURANCE_BUILD_COMPANY_GROUP__;
 })(typeof window !== "undefined" ? window : (typeof global !== "undefined" ? global : this));
