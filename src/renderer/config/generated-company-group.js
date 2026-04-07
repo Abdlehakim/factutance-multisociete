953e7263fb299b60ddd2176b81b2364f73a301ba
unknown <hakimsabkhi001@gmail.com>
2026-04-07 12:42:56 +0100

    update

--- src/renderer/config/generated-company-group.js
+++ src/renderer/config/generated-company-group.js
@@ -1,5 +1,5 @@
 // AUTO-GENERATED. Do not commit.
 (function(g){
-  g.__FACTURANCE_BUILD_COMPANY_GROUP__ = {"mode":"group","groupId":"smartwebify-groupe","groupName":"SmartWebify Groupe","companies":["SmartWebify","Facturance"],"createdAt":"2026-04-07T10:11:38.021Z"};
+  g.__FACTURANCE_BUILD_COMPANY_GROUP__ = {"mode":"group","groupId":"wellcomgroup","groupName":"WELLCOM GROUP","companies":["WELLCOM","ProDisplay"],"createdAt":"2026-04-07T11:25:33.766Z"};
   if (typeof module !== "undefined" && module.exports) module.exports = g.__FACTURANCE_BUILD_COMPANY_GROUP__;
 })(typeof window !== "undefined" ? window : (typeof global !== "undefined" ? global : this));
